e3fee3d2f1d2deed14957e70504375f0fdf45671
Robert Gazdziak <gazdzia2@illinois.edu>
2015-04-19 08:01:13 -0500

    Projectile Scripts added

---
+++ Squerrilla Warfare/Assets/Code/Weapons/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class Projectile : MonoBehaviour {
+
+    public float MaxDamage = 100f;      //Base Damage for direct hits
+    public float DamageRange = 3f;      //Max Range for explosion
+    public float ExplosiveLift = 1f;
+    public bool IsGrenade = false;
+    public float ExplosionTime = 2f;
+
+    public void Start() {
+        Destroy(gameObject, ExplosionTime);
+    }
+
+
+    public void OnCollisionEnter(Collision collision) {
+        //Explosion Animation
+
+        Vector3 explosionLocation = transform.position; //Location for Explosion
+
+        //Get enemies within range of explosion
+        Collider[] colliders = Physics.OverlapSphere(explosionLocation, DamageRange);
+
+        //Check all colliders
+        foreach (Collider c in colliders.Where(c => c.attachedRigidbody))
+        {
+            //Deal Damage
+
+            //Squirrell enemyPlayer = c.GetComponent<Squirrell>();
+            //enemyPlayer.damage(getProjectileDamage());
+
+            //Add Explosion Force?
+            //c.attachedRigidbody.AddExplosionForce(MaxDamage, explosionLocation, DamageRange, ExplosiveLift);
+
+            //Destroy Projectile
+            if (!IsGrenade)
+                Destroy(gameObject);
+        }
+    }
+}
--- Squerrilla Warfare/Assets/Code/Weapons/ProjectileWeapon.cs
+++ Squerrilla Warfare/Assets/Code/Weapons/ProjectileWeapon.cs
@@ -1,4 +1,29 @@
-﻿public abstract class ProjectileWeapon : Weapon {
-	public override void Fire () {}
-	public abstract string GetProjectileName ();
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ProjectileWeapon : Weapon
+{
+    Rigidbody Projectile;
+    Rigidbody projectile;
+
+    public abstract string ProjectileName;
+
+    public abstract int ProjectileDamage;
+
+    public abstract float ProjectileRange;
+
+    public abstract float DetonationTime;
+
+    public abstract float Velocity;
+
+
+    public override void Fire()
+    {
+        projectile = (Rigidbody) Instantiate(Projectile, Game.assets.squirrell.transform.position, Game.assets.squirrell.transform.rotation);
+        projectile.velocity = transform.TransformDirection(Vector3.forward * Velocity);
+
+    }
+
+    
+	
 }
