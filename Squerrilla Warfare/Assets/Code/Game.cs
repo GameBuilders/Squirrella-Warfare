0c90c0889c2d7b5abc2f85d66f0a7b0b05703905
Asaf <geva2@illinois.edu>
2015-04-18 20:41:34 -0500

    small changes

--- Squerrilla Warfare/Assets/Code/Game.cs
+++ Squerrilla Warfare/Assets/Code/Game.cs
@@ -22,25 +22,30 @@ public class Game : MonoBehaviour {
 	[UsedImplicitly] void OnGUI () {
 		Cursor.visible = showMenu;
 		Cursor.lockState = showMenu ? CursorLockMode.None : CursorLockMode.Locked;
-		if (!NetworkManager.Connected)
-			showMenu = true;
+        if (!NetworkManager.Connected)
+        {
+            showMenu = true;
+            showLoadoutMenu = false;
+        }
 		if (showMenu)
 			networkManager.DrawMenu();
-    if (showLoadoutMenu)
-      //Draw Loadout UI
-      GUI.Box (new Rect (Screen.width/2 - 100, 20, 200, 220), "Loadout");
-      //First Loadout
-      GUI.BeginGroup (new Rect (0,0,Screen.width,Screen.height/3));
-      GUI.Box (new Rect (50, 50, Screen.width/2 - 200, 400), primary_slot);
-      if(GUI.Button (new Rect (20, 180, 20, 40), ""))
-        //Change Weapon
-      if(GUI.Button (new Rect (Screen.width/2 - 120, Screen.height/3 - 90, 20, 40), ""))
-        //Change Weapon
-      GUI.Box (new Rect(Screen.width/2 + 100, 10, Screen.width/2 - 200, 100), "Weapon 1")
-      GUI.Box (new Rect(Screen.width/2 + 100, Screen.height/3 + 150, Screen.width/2 - 200, 300), "Description 1")
-      GUI.EndGroup ();
-      if (GUI.Button (new Rect (Screen.width/2 - 100, Screen.height - 120, 200, 100), "Spawn"))
-		    SpawnSquirrell();
+        if (showLoadoutMenu)
+        {
+            //Draw Loadout UI
+            GUI.Box(new Rect(Screen.width / 2 - 100, 20, 200, 220), "Loadout");
+            //First Loadout
+            GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height / 3));
+            GUI.Box(new Rect(50, 50, Screen.width / 2 - 200, 400), "test");
+            if (GUI.Button(new Rect(20, 180, 20, 40), "")) ;
+            //Change Weapon
+            if (GUI.Button(new Rect(Screen.width / 2 - 120, Screen.height / 3 - 90, 20, 40), "")) ;
+            //Change Weapon
+            GUI.Box(new Rect(Screen.width / 2 + 100, 10, Screen.width / 2 - 200, 100), "Weapon 1");
+            GUI.Box(new Rect(Screen.width / 2 + 100, Screen.height / 3 + 150, Screen.width / 2 - 200, 300), "Description 1");
+            GUI.EndGroup();
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height - 120, 200, 100), "Spawn"))
+                SpawnSquirrell();
+        }
 	}
 	public static void JoinedGame () {
     showMenu = false;
@@ -48,4 +53,5 @@ public class Game : MonoBehaviour {
 	}
 	public static void SpawnSquirrell () {Network.Instantiate(assets.squirrell, Vector3.zero, Quaternion.identity, 0);}
 	public static bool showMenu;
+    public static bool showLoadoutMenu;
 }
