e3deb55ca7c6294e3ff1c1ae62062e1a3523611e
Brad Anderson <bsndrsn2@illinois.edu>
2015-04-19 09:56:11 -0500

    Adds basic sound API

--- Squerrilla Warfare/Assets/Code/Assets.cs
+++ Squerrilla Warfare/Assets/Code/Assets.cs
@@ -1,7 +1,9 @@
-﻿using JetBrains.Annotations;
+using JetBrains.Annotations;
 using UnityEngine;
 
 [UsedImplicitly] public class Assets : MonoBehaviour {
 	[UsedImplicitly] public GameObject squirrell;
     [UsedImplicitly] public GameObject arPrefab;
+
+    [UsedImplicitly] public AudioClip[] explosionSound;
 }
