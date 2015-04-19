4a273959d571c435a7b23417af144c5309cb2592
Eric Sauer <ericmsauer@gmail.com>
2015-04-18 20:44:48 -0500

    Fixed Game.cs UI

--- Squerrilla Warfare/Assets/Code/Game.cs
+++ Squerrilla Warfare/Assets/Code/Game.cs
@@ -20,27 +20,30 @@ public class Game : MonoBehaviour {
 	}
 	// ReSharper disable once InconsistentNaming
 	[UsedImplicitly] void OnGUI () {
-		Cursor.visible = showMenu;
-		Cursor.lockState = showMenu ? CursorLockMode.None : CursorLockMode.Locked;
-		if (!NetworkManager.Connected)
+		Cursor.visible = showMenu || showLoadoutMenu;
+		Cursor.lockState = showMenu || showLoadoutMenu ? CursorLockMode.None : CursorLockMode.Locked;
+		if (!NetworkManager.Connected){
 			showMenu = true;
+      showLoadoutMenu = false;
+    }
 		if (showMenu)
 			networkManager.DrawMenu();
-    if (showLoadoutMenu)
+    if (showLoadoutMenu){
       //Draw Loadout UI
       GUI.Box (new Rect (Screen.width/2 - 100, 20, 200, 220), "Loadout");
       //First Loadout
       GUI.BeginGroup (new Rect (0,0,Screen.width,Screen.height/3));
       GUI.Box (new Rect (50, 50, Screen.width/2 - 200, 400), primary_slot);
-      if(GUI.Button (new Rect (20, 180, 20, 40), ""))
+      if(GUI.Button (new Rect (20, 180, 20, 40), ""));
         //Change Weapon
-      if(GUI.Button (new Rect (Screen.width/2 - 120, Screen.height/3 - 90, 20, 40), ""))
+      if(GUI.Button (new Rect (Screen.width/2 - 120, Screen.height/3 - 90, 20, 40), ""));
         //Change Weapon
-      GUI.Box (new Rect(Screen.width/2 + 100, 10, Screen.width/2 - 200, 100), "Weapon 1")
-      GUI.Box (new Rect(Screen.width/2 + 100, Screen.height/3 + 150, Screen.width/2 - 200, 300), "Description 1")
+      GUI.Box (new Rect(Screen.width/2 + 100, 10, Screen.width/2 - 200, 100), "Weapon 1");
+      GUI.Box (new Rect(Screen.width/2 + 100, Screen.height/3 + 150, Screen.width/2 - 200, 300), "Description 1");
       GUI.EndGroup ();
-      if (GUI.Button (new Rect (Screen.width/2 - 100, Screen.height - 120, 200, 100), "Spawn"))
+      if (GUI.Button (new Rect (Screen.width/2 - 100, Screen.height - 120, 200, 100), "Spawn"));
 		    SpawnSquirrell();
+      }
 	}
 	public static void JoinedGame () {
     showMenu = false;
@@ -48,4 +51,5 @@ public class Game : MonoBehaviour {
 	}
 	public static void SpawnSquirrell () {Network.Instantiate(assets.squirrell, Vector3.zero, Quaternion.identity, 0);}
 	public static bool showMenu;
+  public static bool showLoadoutMenu;
 }
