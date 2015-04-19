52ce12dd5d872222e194b3314736971f97d39079
Eric Sauer <ericmsauer@gmail.com>
2015-04-18 20:09:48 -0500

    started loudout GUI

--- Squerrilla Warfare/Assets/Code/Game.cs
+++ Squerrilla Warfare/Assets/Code/Game.cs
@@ -15,12 +15,15 @@ public class Game : MonoBehaviour {
 		//	SpawnSquirrell();
 	}
 	[UsedImplicitly] void OnGUI () {
-		Cursor.visible = showMenu;
-		Cursor.lockState = showMenu ? CursorLockMode.None : CursorLockMode.Locked;
+    Cursor.visible = showMenu;
+    Cursor.lockState = showMenu ? CursorLockMode.None : CursorLockMode.Locked;
+    //Draw Loadout UI
+    GUI.Box (new Rect (Screen.width/2 - 100, 20, 200, 220), "Loadout");
+    GUI.Box (new Rect (50, 2*Screen.height/3, 
 	}
 	public static void JoinedGame () {
 		SpawnSquirrell();
-		showMenu = false;
+    showMenu = false;
 	}
 	public static void SpawnSquirrell () {Network.Instantiate(assets.squirrell, Vector3.zero, Quaternion.identity, 0);}
 	public static bool showMenu;
