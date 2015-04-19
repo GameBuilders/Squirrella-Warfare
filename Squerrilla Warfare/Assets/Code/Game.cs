9aa2cd08a30860cb62a918c8ab2ead61d37ea258
Eric Sauer <ericmsauer@gmail.com>
2015-04-18 20:28:51 -0500

    Draft of loadout (only primary)

--- Squerrilla Warfare/Assets/Code/Game.cs
+++ Squerrilla Warfare/Assets/Code/Game.cs
@@ -26,13 +26,25 @@ public class Game : MonoBehaviour {
 			showMenu = true;
 		if (showMenu)
 			networkManager.DrawMenu();
-    //Draw Loadout UI
-    GUI.Box (new Rect (Screen.width/2 - 100, 20, 200, 220), "Loadout");
-    GUI.Box (new Rect (50, 2*Screen.height/3, 
+    if (showLoadoutMenu)
+      //Draw Loadout UI
+      GUI.Box (new Rect (Screen.width/2 - 100, 20, 200, 220), "Loadout");
+      //First Loadout
+      GUI.BeginGroup (new Rect (0,0,Screen.width,Screen.height/3));
+      GUI.Box (new Rect (50, 50, Screen.width/2 - 200, 400), primary_slot);
+      if(GUI.Button (new Rect (20, 180, 20, 40), ""))
+        //Change Weapon
+      if(GUI.Button (new Rect (Screen.width/2 - 120, Screen.height/3 - 90, 20, 40), ""))
+        //Change Weapon
+      GUI.Box (new Rect(Screen.width/2 + 100, 10, Screen.width/2 - 200, 100), "Weapon 1")
+      GUI.Box (new Rect(Screen.width/2 + 100, Screen.height/3 + 150, Screen.width/2 - 200, 300), "Description 1")
+      GUI.EndGroup ();
+      if (GUI.Button (new Rect (Screen.width/2 - 100, Screen.height - 120, 200, 100), "Spawn"))
+		    SpawnSquirrell();
 	}
 	public static void JoinedGame () {
-		SpawnSquirrell();
     showMenu = false;
+    showLoadoutMenu = true;
 	}
 	public static void SpawnSquirrell () {Network.Instantiate(assets.squirrell, Vector3.zero, Quaternion.identity, 0);}
 	public static bool showMenu;
